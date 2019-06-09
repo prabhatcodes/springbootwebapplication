b92cc4bd4959b46c9157d0d604e95be46927222d
Prabhat Singh Tomar <iampstomar@gmail.com>
2019-06-09 13:08:01 +0530

    Made Book and Author classes

---
+++ src/main/java/com/gfg/prabhat/springbootwebapplication/model/Book.java
@@ -0,0 +1,54 @@
+package com.gfg.prabhat.springbootwebapplication.model;
+
+public class Book {
+    private String title;
+    private String isbn;
+
+    public Book(String title, String isbn) {
+        this.title = title;
+        this.isbn = isbn;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+}
+
+public class Author{
+    private String firstname;
+    private String lastname;
+
+    public Author(String firstname, String lastname) {
+        this.firstname = firstname;
+        this.lastname = lastname;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+}
+
